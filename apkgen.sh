d4df3b879cb94e23366f72b3669d1fcbf378d6bb
Abhimishra939 <46479769+Abhimishra939@users.noreply.github.com>
2021-02-15 00:06:05 +0530

    Update apkgen.sh

--- apkgen.sh
+++ apkgen.sh
@@ -5,6 +5,5 @@ apktool=$tools/apktool
 chmod +x $C/tools/apktool
 $apktool d source.apk
 zip source.zip $C/source -r
-a=source
 ls
-curl -F document=@"${a}.zip" https://api.telegram.org/bot${TOKEN}/sendDocument?chat_id="${CHAT_ID}"
+curl -F document=@"source.zip" https://api.telegram.org/bot${TOKEN}/sendDocument?chat_id="${CHAT_ID}"
